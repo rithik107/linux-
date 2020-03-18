1903b153b8bb761a0078354344afe624da938991
rithik107 <62349283+rithik107@users.noreply.github.com>
2020-03-18 22:40:46 +0530

    Add files via upload

---
+++ blockedbro.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+int main()
+{
+int pid,status;
+pid=fork();
+if(pid<0)
+{
+printf("error");
+}
+else if(pid==0)
+{
+printf("child terminating\n");
+exit(0);
+}
+if(wait(&status)!=pid)
+printf("wait error\n");
+else
+printf("parent terminating\n");
+}
+
